1a0d928a63a25e2219b1dea7e9188bfbdba1c607
HaKietHung2905 <hakiethung1998@gmail.com>
2025-12-04 09:04:42 +0700

    Updates

--- cards/context_processors.py
+++ cards/context_processors.py
@@ -1,14 +1,21 @@
-from .models import CardSet, OtherProduct
+from .models import CardSet, OtherProduct, CartItem
+from django.db.models import Sum
 
 def card_sets_processor(request):
     """
-    Context processor to make card sets and product types available globally in all templates.
-    This allows the navbar to display all card sets and product categories in dropdown menus.
+    Context processor to make card sets, product types, and cart count available globally.
     """
     card_sets = CardSet.objects.all().order_by('-release_date')
     product_types = OtherProduct.PRODUCT_TYPE_CHOICES
     
+    cart_count = 0
+    if request.user.is_authenticated:
+        # Calculate total items in cart (sum of quantities)
+        result = CartItem.objects.filter(user=request.user).aggregate(total=Sum('quantity'))
+        cart_count = result['total'] or 0
+    
     return {
         'all_card_sets': card_sets,
         'all_product_types': product_types,
+        'cart_count': cart_count,
     }
